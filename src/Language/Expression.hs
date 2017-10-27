20095c019527b2d231ed80fd3e7a2012e986ab02
Ashley Yakeley <ashley@yakeley.org>
2017-10-27 02:10:44 -0700

    Language.Expression module

---
+++ src/Language/Expression.hs
@@ -0,0 +1,17 @@
+module Language.Expression
+(
+    module Language.Expression.Expression,
+    module Language.Expression.Value,
+    module Language.Expression.Match,
+    module Language.Expression.Pattern,
+    module Language.Expression.Regular,
+    module Language.Expression.Mono,
+) where
+{
+    import Language.Expression.Expression;
+    import Language.Expression.Value;
+    import Language.Expression.Match;
+    import Language.Expression.Pattern;
+    import Language.Expression.Regular;
+    import Language.Expression.Mono;
+}
